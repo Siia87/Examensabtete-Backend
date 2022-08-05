ffb5100257742490cb77bbb0823b681b74013e79
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2022-08-05 15:16:08 +0200

    Installed Ecrypt and used this when creating user to protect the password and using this when user is signing in to compare password

--- Routes/login.js
+++ Routes/login.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from 'cors'
+
 import Users from "../models/usersModel.js"
 const appLogin = express.Router()
 
@@ -8,19 +9,27 @@ appLogin.use(express.urlencoded({ extended: true }))
 appLogin.use(express.json())
 appLogin.post("/", async (req, res) => {
   console.log(req.body)
-
-  const user = await Users.findOne({
-    $and: [
-      { email: req.body.data.email },
-      { password: req.body.data.password }
-    ]
-
-  })
-  console.log(user)
   try {
-    res.send(user);
-  } catch (error) {
+    const user = await Users.findOne({
+      email: req.body.data.email
+    })
+
+    if (user) {
+      const validPassword = await bcrypt.compare(body.password, user.password)
+
+      if (validPassword) {
+        res.send(user._id)
+        res.status(200).json({ message: "Valid password" });
+      } else {
+        res.status(400).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ error: "User does not exist" });
+    }
+  }
+  catch (error) {
     res.status(500).send(error)
   }
-})
+});
+
 export default appLogin
--- Routes/users.js
+++ Routes/users.js
@@ -1,37 +1,31 @@
 import express from "express"
-import cors from 'cors'
+import bcrypt from "bcryptjs"
+
 import Users from "../models/usersModel.js"
 const appUser = express.Router()
 
-appUser.use(cors())
+
 appUser.use(express.urlencoded({ extended: true }))
 appUser.use(express.json())
 
-appUser.post("/", async (req, res) => {
-  console.log(req.body)
-
-  const user = await Users.findOne({
-    $and: [
-      { email: req.body.email },
-      { password: req.body.password }
-    ]
-
-  })
-
-  try {
-    res.send(user._id);
-  } catch (error) {
-    res.status(500).send(error)
-  }
-})
 
 
 appUser.post("/", async (req, res) => {
   console.log(req.body)
-  const user = new Users(req.body)
-  console.log(user)
   try {
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    console.log(hashPassword)
+    const user = new Users({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashPassword
+    });
+
     await user.save()
+    res.status(200)
     res.send(user)
   } catch (error) {
     res.status(500).send(error)
